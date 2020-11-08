190bb9ec9c91d39177d8200eb6b66e8457f202a4
Cari Borton <cborton@ga-p5.msk.21-school.ru>
2020-11-08 15:39:35 +0300

    split_01

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: cborton <cborton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 14:38:18 by cborton           #+#    #+#             */
-/*   Updated: 2020/11/08 15:06:49 by cborton          ###   ########.fr       */
+/*   Updated: 2020/11/08 15:38:02 by cborton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,43 @@ int		numb_str(const char *s, const char c)
 	return(n);
 }
 
+static int get_len_str(char const *s, char c, int str_id)
+{
+	int n;
+
+	n = 0;
+	while (s[str_id] && s[str_id] == c)
+		str_id++;
+	while (s[str_id] && s[str_id] != c)
+	{
+		str_id++;
+		n++;
+	}
+	return (n + 1);
+}
+
+static char **ft_another_function(char const *s, char c, char **res)
+{
+	int str_id;
+	int len_str;
+	int	i;
+
+	str_id = 0;
+	i = 0;
+	while (s[str_id])
+	{
+		len_str = get_len_str(s, c, str_id);
+		if (!(res[i] = (char *)malloc(sizeof(char) * n)))
+		{
+			while (i)
+				free(res[i -=1]);
+			free(res);
+			return(NULL);
+		}
+	}
+	
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
@@ -43,7 +80,14 @@ char	**ft_split(char const *s, char c)
 	
 	if (!s)
 		return (NULL);
-	n = numb_str(s, c);
+	n = numb_str(s, c) + 1;
+	res = (char **)malloc(sizeof(char *) * n);
+	if (!res)
+		return (NULL);
+	res[n - 1] = NULL;
+	if (n == 1)
+		return(res);
+	res = ft_another_function(s, c, res)
 	return (res);
 }
 
